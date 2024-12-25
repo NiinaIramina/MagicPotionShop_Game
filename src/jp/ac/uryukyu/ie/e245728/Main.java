6634f0db0b1fc30999fb00228d6149dd007b60c3
Niina Iramina <e245728@ie.u-ryukyu.ac.jp>
2024-12-25 19:37:57 +0900

    デモの作成。プレゼントタイム

--- src/jp/ac/uryukyu/ie/e245728/Main.java
+++ src/jp/ac/uryukyu/ie/e245728/Main.java
@@ -1,8 +1,37 @@
 package jp.ac.uryukyu.ie.e245728;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+
         System.out.println("『魔法の薬、売ります。』");
+        System.out.println("あなたの名前を教えてください。");
+
+        Scanner scan = new Scanner(System.in);//どこに置いてもいい。ただし、scanの前に書く必要がある。
+        String name = scan.next();
+
+        System.out.println("「" + name + "」さん、このゲームへようこそ！");
+
+        while(true){
+        System.out.println("あなたの好きな素材を教えてください。好きなだけプレゼントします。");
+        System.out.println("1.スライムのジェル");
+        System.out.println("2.聖水");
+        System.out.println("3.もういらない！");
+
+        int item_num = scan.nextInt();
+        
+        if (item_num == 1){
+            System.out.println("あなたはスライムのジェルを受け取った。プルプルしているね！");
+        } else if (item_num == 2){
+            System.out.println("あなたは聖水を受け取った。なんだか神聖な気がする…。");
+        } else if (item_num == 3){ //３の時ブレイクしてwhileから抜ける
+            System.out.println("プレゼントタイムを終了しました。");
+            break;
+        }
+        }
+        System.out.println("ゲームを終了します。");
+
+        scan.close();
     }
 
 }
