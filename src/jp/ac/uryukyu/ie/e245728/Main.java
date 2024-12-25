3894eaba549259282fcb20fd1237a9ee7d65cc79
Niina Iramina <e245728@ie.u-ryukyu.ac.jp>
2024-12-25 20:38:43 +0900

    シーンを関数で管理する

--- src/jp/ac/uryukyu/ie/e245728/Main.java
+++ src/jp/ac/uryukyu/ie/e245728/Main.java
@@ -12,26 +12,49 @@ public class Main {
 
         System.out.println("「" + name + "」さん、このゲームへようこそ！");
 
+        String fristseen = "プレゼントシーン";
+        String seen = fristseen;
+
         while(true){
+            if (seen == "プレゼントシーン"){
+                seen = presentseen(scan);
+            }
+
+            else if (seen == "ゲーム終了シーン"){
+                break;
+            }
+        }
+        System.out.println("ゲームを終了します。");
+
+        scan.close();
+    }
+
+    public static String presentseen(Scanner scan){
         System.out.println("あなたの好きな素材を教えてください。好きなだけプレゼントします。");
         System.out.println("1.スライムのジェル");
         System.out.println("2.聖水");
         System.out.println("3.もういらない！");
 
         int item_num = scan.nextInt();
+
+        String nextseen = "";
         
         if (item_num == 1){
             System.out.println("あなたはスライムのジェルを受け取った。プルプルしているね！");
+            nextseen = "プレゼントシーン";
         } else if (item_num == 2){
             System.out.println("あなたは聖水を受け取った。なんだか神聖な気がする…。");
+            nextseen = "プレゼントシーン";
         } else if (item_num == 3){ //３の時ブレイクしてwhileから抜ける
             System.out.println("プレゼントタイムを終了しました。");
-            break;
+            nextseen = "ゲーム終了シーン";
         }
-        }
-        System.out.println("ゲームを終了します。");
 
-        scan.close();
+        return nextseen;
+    }
+
+    public static void garage(){
+        System.out.println("倉庫にしまいましょう！");
     }
 
 }
